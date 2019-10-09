eaa8e03f20c647ae0c49cfa1db615364dbef21ad
nur <nur.php@gmail.com>
2019-10-09 23:57:30 +0800

    Updates

---
+++ precache-manifest.c088e7b398eaa39312577154d4c9bb6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e7c31422646b16cb749f554312874de",
+    "url": "/IMI_ASSIGNMENT_2/index.html"
+  },
+  {
+    "revision": "5d0b90ab7bf81f55e948",
+    "url": "/IMI_ASSIGNMENT_2/static/css/2.5ab9459b.chunk.css"
+  },
+  {
+    "revision": "277a0d825f27f5ead339",
+    "url": "/IMI_ASSIGNMENT_2/static/css/main.ca2bc889.chunk.css"
+  },
+  {
+    "revision": "5d0b90ab7bf81f55e948",
+    "url": "/IMI_ASSIGNMENT_2/static/js/2.d1655a7a.chunk.js"
+  },
+  {
+    "revision": "277a0d825f27f5ead339",
+    "url": "/IMI_ASSIGNMENT_2/static/js/main.6722a022.chunk.js"
+  },
+  {
+    "revision": "e0386025761b98d02cd5",
+    "url": "/IMI_ASSIGNMENT_2/static/js/runtime-main.d7f66942.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/IMI_ASSIGNMENT_2/static/media/logo.25bf045c.svg"
+  }
+]);
